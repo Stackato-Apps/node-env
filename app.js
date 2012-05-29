cad78db996e03a5d4239442329624a72b7635a7b
Jamie Paton <jamie.paton@googlemail.com>
2012-05-28 17:15:06 -0700

    Make this app less of an ugly duckling.

--- app.js
+++ app.js
@@ -6,12 +6,14 @@ var port = process.env.VCAP_APP_PORT || 1337;
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write('<title>Stackato environment variables</title>');
-  res.write('<h1>Stackato environment variables</h1>');
+  res.write('<h1>Stackato environment variables</h1><hr/>');
   res.write('<table>');
   for (var env in process.env){
-    res.write('<tr><td style="text-align: right; vertical-align: top; padding: 4px 1em; "><b>'
+    res.write('<tr><td style="border-style: solid;\
+                              border-color: #034569;background-color: #9FB6C3;\
+                              text-align: right; vertical-align: top; padding: 4px 1em; "><b>'
               + env + '</b></td>');
-    res.write('<td><tt style="font-family: Monaco, Consolas, monospace;">'
+    res.write('<td><tt style="padding-left: 4px">'
               + process.env[env] + '</tt></td></tr>');
   }
   res.write('</table>');
